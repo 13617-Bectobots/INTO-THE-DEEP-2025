764169e4036c7f1ad04ae23caeba283014412b41
beckyistimegod <bectobots@gmail.com>
2025-02-09 04:34:18 -0500

    updating tuning values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -81,9 +81,9 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 4.3;
+        public double lateralGain = 3.5;
+        public double headingGain = 7.4; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
